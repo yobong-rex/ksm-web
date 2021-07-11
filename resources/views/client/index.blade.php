87f4c2281a27e62762e8ae498ccf7be0c597b1b8
rickyandrean99 <ricky.ralph09@gmail.com>
2021-07-11 20:05:10 +0700

    Template Layout Done
    Main Page Content on Progress

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,5 +1,47 @@
 @extends('layout.client')
 
 @section('content')
+    <section>
+        <div class="container" id="introduction" style="height: 700px;">
+            <div class="introduction-wrapper pt-4 h4 text-white">
+                Section ini berisi tentang introduction ksm
+            </div>
+        </div>
+    </section>
 
+    <section>
+        <div class="container" style="height: 700px">
+            <div class="p-5" id="struktur-organisasi"></div>
+            <div class="struktur-organisasi-wrapper h4 text-white">
+                Section ini berisi tentang struktur organisasi
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container" style="height: 700px">
+            <div class="p-5" id="acara"></div>
+            <div class="acara-wrapper h4 text-white">
+                Section ini berisi tentang acara
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container" style="height: 700px">
+            <div class="p-5" id="galeri"></div>
+            <div class="galeri-wrapper h4 text-white">
+                Section ini berisi tentang galeri
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container" style="height: 700px">
+            <div class="p-5" id="lsta-bursa"></div>
+            <div class="lsta-bursa-wrapper h4 text-white">
+                Section ini berisi tentang lsta dan bursa
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/layout/client.blade.php
+++ resources/views/layout/client.blade.php
@@ -18,11 +18,12 @@
 
         <!-- Font Resources -->
         <link rel="stylesheet" href="{{ asset('assets/css/nucleo-icons.css') }}">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
 
     <body>
         <!-- Container Website -->
-        <div class="container-fluid wrapper">
+        <div class="container-fluid wrapper" id="#">
             <!-- Header Website -->
             <header>
                 <div class="header-wrapper text-center">
@@ -34,37 +35,62 @@
 
                     <div class="header-motto font-weight-bold font-italic text-white">WE NOT ME</div>
 
-                    <div><button type="button" class="btn btn-primary font-weight-bold btn-get-started">GET STARTED</button></div>
+                    <div><a href="#navigation-bar"><button type="button" class="btn btn-primary font-weight-bold btn-get-started">GET STARTED</button></a></div>
                 </div>
             </header>
 
             <!-- Navigation Website -->
-            <nav class="navbar navbar-expand-lg bg-primary" id="navigation-bar">
+            <nav class="navbar navbar-expand-lg bg-primary sticky-top" id="navigation-bar" style="margin-bottom: 10px;">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">KSM Informatika</a>
 
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <!-- Collapse button for trigger mobile -->
+                    <button onclick="openNav()" style="background: none; border: 0; outline: 0" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-bar navbar-kebab"></span>
                         <span class="navbar-toggler-bar navbar-kebab"></span>
                         <span class="navbar-toggler-bar navbar-kebab"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <!-- Navigation for desktop view -->
+                    <div class="collapse navbar-collapse navigation-wrapper" id="navbarTogglerDemo02">
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0 navigation-list">
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Beranda</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Struktur Organisasi</a>
+                                <a class="nav-link" href="#struktur-organisasi">Struktur Organisasi</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Acara</a>
+                                <a class="nav-link" href="#acara">Acara</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Galeri</a>
+                                <a class="nav-link" href="#galeri">Galeri</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">LSTA & BURSA</a>
+                                <a class="nav-link" href="#lsta-bursa">LSTA & BURSA</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Navigation for mobile view -->
+                    <div class="side-navigation bg-dark">
+                        <i class="large material-icons text-white btn-close" onclick="closeNav()" style="cursor: pointer">close</i>
+                        
+                        <ul class="mr-auto mt-2 mt-lg-0 side-nav-list">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Beranda</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#struktur-organisasi">Struktur Organisasi</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#acara">Acara</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#galeri">Galeri</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#lsta-bursa">LSTA & BURSA</a>
                             </li>
                         </ul>
                     </div>
@@ -73,22 +99,79 @@
 
             <!-- Content Website -->
             <main>
-                <div class="container" style="background: red;height: 1000px">
+                <div class="container-fluid">
                     @yield("content")
-                    asd<br><br><br><br><br><br>
-                    hrtf<br><br><br><br><br><br>
-                    qwe<br><br><br><br><br><br>
-                    mgh<br><br><br><br><br><br>
-                    sad<br><br><br><br><br><br>
-                    vcx<br><br><br><br><br><br>
-                    ytu<br><br><br><br><br><br>
-                    gsfd<br><br><br><br><br><br>
-                    jasd<br><br><br><br><br><br>
                 </div>
             </main>
 
             <!-- Footer Website -->
-            <footer></footer>
+            <footer>
+                <div class="container-fluid bg-dark">
+                    <div class="container footer-wrapper p-3 pt-5 p-sm-1">
+                        <div class="row footer-section text-white">
+                            <div class="col-12 col-sm-12 col-md-6 order-2 order-sm-2 order-md-1 text-left p-3 p-sm-3 p-md-5">
+                                <div class="h3 font-weight-bold text-white footer-content-header">Alamat</div>
+                                <div class="footer-content-text">Gedung TC 2.3 Universitas Surabaya</div>
+                                <div class="footer-content-text">Jl. Raya Kali Rungkut, Surabaya, Jawa Timur (60293)</div>
+                            </div>
+
+                            <div class="col-12 col-sm-12 col-md-6 order-4 order-sm-4 order-md-2 text-left text-sm-left text-md-right p-3 p-sm-3 p-md-5">
+                                <div class="h3 font-weight-bold text-white footer-content-header">Sosial Media</div>
+                                <div>
+                                    <a href="https://www.youtube.com/channel/UCkIk0gaY-TXMfdTHpFGZJAw" target="_blank"><img src="{{ asset('assets/img/sosial_media/youtube.png') }}" alt="youtube" class="social-media-icon"></a>
+                                    <a href="https://wa.me/6281353067152" target="_blank"><img src="{{ asset('assets/img/sosial_media/whatsapp.png') }}" alt="whatsapp" class="social-media-icon"></a>
+                                    <a href="https://line.me/ti/p/@rvy2022m" target="_blank"><img src="{{ asset('assets/img/sosial_media/line.png') }}" alt="line" class="social-media-icon"></a>
+                                    <a href="https://www.instagram.com/ksm_informatika/" target="_blank"><img src="{{ asset('assets/img/sosial_media/instagram.png') }}" alt="instagram" class="social-media-icon"></a>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-sm-12 col-md-6 order-3 order-sm-3 order-md-3 text-left p-3 p-sm-3 p-md-5">
+                                <div class="h3 font-weight-bold text-white footer-content-header">Email</div>
+                                <div class="footer-content-text">ksm.if.ubaya@gmail.com</div>
+                            </div>
+
+                            <div class="col-12 col-sm-12 col-md-6 order-1 order-sm-1 order-md-4 text-center text-sm-center text-md-right p-3 p-sm-3 p-md-5">
+                                <img src="{{ asset('assets/img/ksm-putih.png') }}" alt="logo-footer" class="logo-footer w-50">
+                                <!-- <div class="clearfix"></div> -->
+                                <!-- <div class="mt-3 text-center w-md-50 bg-primary" id="easter-egg"></div> -->
+                                <div class="row">
+                                    <div class="col-0 col-sm-0 col-md-6"></div>
+                                    <div class="col-12 col-sm-12 col-md-6 text-center" id="easter-egg">:)</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row footer-section">
+                            <div class="col-12 text-white h4 text-center p-4" id="copyright">
+                                Copyright ©2021 | KSM Informatika Universitas Surabaya
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+            <!-- Javascript & Jquery -->
+            <script>
+                $(document).ready(function() {
+                    $('#easter-egg').css('visibility', 'hidden');
+                });
+
+                $(document).on('mouseover', '.logo-footer', function() {
+                    $('#easter-egg').css('visibility', 'visible');
+                });
+
+                $(document).on('mouseleave', '.logo-footer', function() {
+                    $('#easter-egg').css('visibility', 'hidden');
+                });
+
+                function openNav() {
+                    $('.side-navigation').css('width', '250px');
+                }
+
+                function closeNav() {
+                    $('.side-navigation').css('width', '0');
+                }
+            </script>
         </div>
     </body>
 </html>
